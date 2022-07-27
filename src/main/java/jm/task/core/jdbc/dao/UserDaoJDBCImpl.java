a513ffe9674efa62dc70b8dea3002dc5cc26e48f
Anatoliy Girak <agirak1@rambler.ru>
2022-07-27 16:54:46 +0300

    Initial commit

---
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -0,0 +1,132 @@
+package jm.task.core.jdbc.dao;
+
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+// Обработка всех исключений, связанных с работой с базой данных должна находиться в dao
+public class UserDaoJDBCImpl implements UserDao {
+    public UserDaoJDBCImpl() {
+
+    }
+    // Создание таблицы для User(ов) - не должна приводить к исключению, если такая таблица уже существует
+    public void createUsersTable() {
+        String s = "CREATE TABLE IF NOT EXISTS users (id mediumint NOT NULL auto_increment, name varchar(50) NOT NULL, lastName varchar(60) NOT NULL, age tinyint, primary key(id))";
+        try (Connection connection = Util.getConnection();) {
+            try (Statement statement = connection.createStatement();) {
+                connection.setAutoCommit(false);
+                statement.executeUpdate(s);
+                System.out.println("Table was created");
+                connection.commit();
+            } catch (SQLException e) {
+                connection.rollback();
+                e.printStackTrace();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+    }
+    // Удаление таблицы User(ов) - не должна приводить к исключению, если таблицы не существует
+    public void dropUsersTable() {
+        String s = "DROP TABLE if exists users";
+
+        try (Connection connection = Util.getConnection();) {
+            try (Statement statement = connection.createStatement();) {
+                statement.executeUpdate(s);
+                System.out.println("Table was deleted");
+                connection.commit();
+            } catch (SQLException ex) {
+                connection.rollback();
+                ex.printStackTrace();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+    }
+    // Добавление User в таблицу
+    public void saveUser(String name, String lastName, byte age) {
+        String s = "insert into users (name, lastName, age) values (?, ?, ?)";
+        try (Connection connection = Util.getConnection();) {
+            try (PreparedStatement preparedStatement = connection.prepareStatement(s);) {
+                preparedStatement.setString(1, name);
+                preparedStatement.setString(2, lastName);
+                preparedStatement.setByte(3, age);
+                preparedStatement.executeUpdate();
+                System.out.println("Пользователь: " + name + " добавлен в базу данных");
+                connection.commit();
+            } catch (SQLException ex) {
+                connection.rollback();
+                ex.printStackTrace();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+    }
+    // Удаление User из таблицы (по id)
+    public void removeUserById(long id) {
+        String s = "DELETE FROM users WHERE id = ?";
+        try (Connection connection = Util.getConnection();) {
+            try (PreparedStatement preparedStatement = connection.prepareStatement(s);) {
+                preparedStatement.setLong(1, id);
+                preparedStatement.executeUpdate();
+                System.out.println("Пользователь: " + id + " удален из БД");
+                connection.commit();
+            } catch (SQLException ex) {
+                connection.rollback();
+                ex.printStackTrace();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+    }
+    // Получение всех User(ов) из таблицы
+    public List<User> getAllUsers() throws SQLException {
+        List<User> list = null;
+        try (Statement statement = Util.getConnection().createStatement()) {
+            ResultSet resultSet = statement.executeQuery("SELECT name, lastName, age FROM users");
+            list = new ArrayList<>();
+
+            while (resultSet.next()) {
+                User user = new User();
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("lastName"));
+                user.setAge(resultSet.getByte("age"));
+                list.add(user);
+            }
+            System.out.println(list);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+    // Очистка содержания таблицы
+    public void cleanUsersTable() {
+        try (Connection connection = Util.getConnection();) {
+            try (Statement statement = connection.createStatement();) {
+                statement.executeUpdate("Truncate Table Users");
+                System.out.println("Table deleted");
+                connection.commit();
+            } catch (SQLException e) {
+                connection.rollback();
+                e.printStackTrace();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,35 @@
+package jm.task.core.jdbc.util;
+
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+    // реализуйте настройку соединения с БД
+    private static final String url = "jdbc:mysql://localhost:3306/mysql";
+    private static final String username = "root";
+    private static final String password = "Fred_1304";
+
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url, username, password);
+            connection.setAutoCommit(false);
+            System.out.println("Successfully connected");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static SessionFactory getSessionFactory() {
+        Configuration configuration = new Configuration().addAnnotatedClass(User.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        return sessionFactory;
+    }
+}
